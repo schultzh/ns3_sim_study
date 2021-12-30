c0f185f871c7ac15b5291f9dce4fcdc998f6b518
schultzh <42979565+schultzh@users.noreply.github.com>
2021-12-30 15:42:45 -0500

    Add source code comments to sort.py

--- data-formatting/sort.py
+++ data-formatting/sort.py
@@ -7,12 +7,14 @@ def main():
     
     flows = {}
 
-    with open("meshFlow.json", "r") as fh:
-        data = json.load(fh)["FlowMonitor"]
-    
+    with open("meshFlow.json", "r") as fh: #input json file name
+        data = json.load(fh)["FlowMonitor"] #load top dict
+        
+    #combine subdicts
     data = data["Ipv4FlowClassifier"]["Flow"], data["FlowStats"]["Flow"]
 
-    for flow in data: # <-- P2
+    #create nested dicts
+    for flow in data: 
         if flow["@flowId"] not in flows.keys():
             flows[flow["@flowId"]] = {
                 "packets": [],
@@ -21,13 +23,14 @@ def main():
             
         f = Flow(flow)
         
+        #append each dict
         try:
             getattr(f,"sourceaddress")
             flows[flow["@flowId"]]["addresses"].append(dict(f))
         except AttributeError:
             flows[flow["@flowId"]]["packets"].append(dict(f))
     
-    with open("flows.yaml","w") as fh:
+    with open("flows.yaml","w") as fh: # output file
         yaml.dump(flows,fh,indent=2)
     
 if __name__=="__main__":
