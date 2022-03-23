6d43796d188e9a0678931a90a4950f4cee95aa2b
schultzh <schultzh@allegheny.edu>
2022-03-23 11:54:56 -0400

    add variant simulations

---
+++ variants/WAP-NewReno.cc
@@ -0,0 +1,181 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "ns3/config.h"
+#include "ns3/string.h"
+#include "ns3/log.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/ssid.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mobility-helper.h"
+#include "ns3/on-off-helper.h"
+#include "ns3/yans-wifi-channel.h"
+#include "ns3/mobility-model.h"
+#include "ns3/packet-sink.h"
+#include "ns3/packet-sink-helper.h"
+#include "ns3/internet-stack-helper.h"
+#include "ns3/ipv4-address-helper.h"
+#include "ns3/ipv4-global-routing-helper.h"
+
+#include "ns3/flow-monitor.h"
+#include "ns3/flow-monitor-helper.h"
+#include "ns3/flow-monitor-module.h"
+
+#include "ns3/netanim-module.h"
+
+
+NS_LOG_COMPONENT_DEFINE ("wifi-NewReno");
+
+using namespace ns3;
+
+Ptr<PacketSink> sink;                         /* Pointer to the packet sink application */
+uint64_t lastTotalRx = 0;                     /* The value of the last total received bytes */
+
+std::string datetime()
+{
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer[80];
+
+    time (&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer,80,"%d-%m-%Y %H-%M-%S",timeinfo);
+    return std::string(buffer);
+}
+
+void
+CalculateThroughput ()
+{
+  std::ofstream avgThroughput;
+  avgThroughput.open("NR_throughput.csv", std::ios_base::app);
+  Time now = Simulator::Now (); 
+  double cur = (sink->GetTotalRx () - lastTotalRx) * (double) 8 / 1e5;     /* Convert Application RX Packets to MBits. */
+  avgThroughput << now.GetSeconds () << "," << cur << ", Mbit/s" << std::endl;
+  lastTotalRx = sink->GetTotalRx ();
+  Simulator::Schedule (MilliSeconds (100), &CalculateThroughput);
+  avgThroughput.close();
+} 
+
+int
+main (int argc, char *argv[])
+{
+
+  uint32_t payloadSize = 1472;                       /* Transport layer payload size in bytes. */
+  std::string dataRate = "100Mbps";                  /* Application layer datarate. */  
+  std::string phyRate = "HtMcs7";                    /* Physical layer bitrate. */
+  double simulationTime = 30;
+
+
+/* Configure TCP Options */
+  Config::SetDefault ("ns3::TcpL4Protocol::SocketType", TypeIdValue (TypeId::LookupByName ("ns3::TcpNewReno")));
+  Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (payloadSize));
+
+  WifiMacHelper wifiMac;
+  WifiHelper wifiHelper;
+  wifiHelper.SetStandard (WIFI_STANDARD_80211n_5GHZ);
+
+  /* Set up Legacy Channel */
+  YansWifiChannelHelper wifiChannel;
+  wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel", "Frequency", DoubleValue (5e9));
+
+  /* Setup Physical Layer */
+  YansWifiPhyHelper wifiPhy;
+  wifiPhy.SetChannel (wifiChannel.Create ());
+  wifiPhy.SetErrorRateModel ("ns3::YansErrorRateModel");
+  wifiHelper.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+                                      "DataMode", StringValue (phyRate),
+                                      "ControlMode", StringValue ("HtMcs0"));
+
+  NodeContainer APNodes;
+  APNodes.Create (1);
+
+  NodeContainer wifiNodes;
+  wifiNodes.Create (25);
+
+  /* Configure AP */
+  Ssid ssid = Ssid ("network");
+  wifiMac.SetType ("ns3::ApWifiMac",
+                   "Ssid", SsidValue (ssid));
+
+  NetDeviceContainer apDevice;
+  apDevice = wifiHelper.Install (wifiPhy, wifiMac, APNodes);
+
+  /* Configure STA */
+  wifiMac.SetType ("ns3::StaWifiMac",
+                   "Ssid", SsidValue (ssid));
+
+  NetDeviceContainer staDevices;
+  staDevices = wifiHelper.Install (wifiPhy, wifiMac, wifiNodes);
+
+  /* Mobility model */
+  MobilityHelper mobility;
+
+mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+  "MinX", DoubleValue (0.0),
+  "MinY", DoubleValue (0.0),
+  "DeltaX", DoubleValue (100.0),
+  "DeltaY", DoubleValue (100.0),
+  "GridWidth", UintegerValue (5),
+  "LayoutType", StringValue ("RowFirst"));
+  
+  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+  "Bounds", RectangleValue (Rectangle (-10000, 10000, -10000, 10000)));
+  mobility.Install (wifiNodes);
+
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (APNodes);
+  
+
+  /* Internet stack */
+  InternetStackHelper stack;
+  stack.Install (APNodes);
+  stack.Install (wifiNodes);
+
+  Ipv4AddressHelper address;
+  address.SetBase ("10.0.0.0", "255.255.255.0");
+  Ipv4InterfaceContainer apInterface;
+  apInterface = address.Assign (apDevice);
+  Ipv4InterfaceContainer staInterface;
+  staInterface = address.Assign (staDevices);
+
+  /* Populate routing table */
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  /* Install TCP Receiver on the access point */
+  PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), 8080));
+  ApplicationContainer sinkApp = sinkHelper.Install (APNodes);
+  sink = StaticCast<PacketSink> (sinkApp.Get (0));
+
+  /* Install TCP/UDP Transmitter on the station */
+  OnOffHelper server ("ns3::TcpSocketFactory", (InetSocketAddress (apInterface.GetAddress (0), 8080)));
+  server.SetAttribute ("PacketSize", UintegerValue (payloadSize));
+  server.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  server.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+  server.SetAttribute ("DataRate", DataRateValue (DataRate (dataRate)));
+  ApplicationContainer serverApp = server.Install (wifiNodes);
+
+  /* Start Applications */
+  sinkApp.Start (Seconds (0.0));
+  serverApp.Start (Seconds (1.0));
+  Simulator::Schedule (Seconds (1.1), &CalculateThroughput);
+
+  // Create Flow Monitor
+  FlowMonitorHelper flowMon;
+	Ptr<FlowMonitor> monitor = flowMon.InstallAll();
+
+  /* Start Simulation */
+  Simulator::Stop (Seconds (simulationTime + 1));
+  Simulator::Run ();
+
+  monitor->SerializeToXmlFile(datetime()+"_NR_FlowMon.xml", true, true);
+  AnimationInterface anim(datetime()+"_NR.xml");
+
+  Simulator::Destroy ();
+  
+  return 0;
+}
